5abbd862d4b7d56fc01b4873922aa93ffd075516
idan guindy <idanguindy@gmail.com>
2026-03-26 00:36:44 +0200

    auto: claude update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './lib/AuthContext';
-import Layout from './components/Layout';
 
-import Home from './pages/Home';
-import Marketplace from './pages/Marketplace';
-import BotProfile from './pages/BotProfile';
-import MyDashboard from './pages/MyDashboard';
-import CreateBot from './pages/CreateBot';
-import EditBot from './pages/EditBot';
-import UserProfile from './pages/UserProfile';
-import OrdersManager from './pages/OrdersManager';
-import ProUpgrade from './pages/ProUpgrade';
-import ExpertsEdit from './pages/ExpertsEdit';
-import Contact from './pages/Contact';
-import Privacy from './pages/Privacy';
-import Terms from './pages/Terms';
-import Admin from './pages/Admin';
+import HomePage from './pages/HomePage';
+import BuilderPage from './pages/BuilderPage';
+import CardPage from './pages/CardPage';
+import DashboardPage from './pages/DashboardPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Marketplace" element={<Marketplace />} />
-            <Route path="/BotProfile" element={<BotProfile />} />
-            <Route path="/MyDashboard" element={<MyDashboard />} />
-            <Route path="/CreateBot" element={<CreateBot />} />
-            <Route path="/EditBot" element={<EditBot />} />
-            <Route path="/UserProfile" element={<UserProfile />} />
-            <Route path="/OrdersManager" element={<OrdersManager />} />
-            <Route path="/ProUpgrade" element={<ProUpgrade />} />
-            <Route path="/ExpertsEdit" element={<ExpertsEdit />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Privacy" element={<Privacy />} />
-            <Route path="/PrivacyPolicy" element={<Privacy />} />
-            <Route path="/Terms" element={<Terms />} />
-            <Route path="/Admin" element={<Admin />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/builder" element={<BuilderPage />} />
+          <Route path="/builder/:cardId" element={<BuilderPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/c/:slug" element={<CardPage />} />
+          {/* Fallback */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
   );
